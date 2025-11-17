a59b4dc090f26d25ea88d9d8fb87ff1672585782
OziRis78 <oguzhantg3@gmail.com>
2025-11-18 01:20:26 +0300

    first design with a basic math example

--- src/test/java/com/twistral/konoblo/Test.java
+++ src/test/java/com/twistral/konoblo/Test.java
@@ -15,11 +15,162 @@
 package com.twistral.konoblo;
 
 
+import java.util.Scanner;
+
 public class Test {
 
+    public static void mul(KonobloConsole cns) {
+        int x = cns.readInt("Enter number #1: ");
+        int y = cns.readInt("Enter number #2: ");
+        cns.printf("%d * %d = %d\n", x, y, x*y);
+    }
+
+    public static void add(KonobloConsole cns) {
+        int x = cns.readInt("Enter number #1: ");
+        int y = cns.readInt("Enter number #2: ");
+        cns.printf("%d + %d = %d\n", x, y, x+y);
+    }
+
+    public static void sub(KonobloConsole cns) {
+        int x = cns.readInt("Enter number #1: ");
+        int y = cns.readInt("Enter number #2: ");
+        cns.printf("%d - %d = %d\n", x, y, x-y);
+    }
+
+    public static void mainMenu(KonobloConsole cns) {
+        cns.println("Hello please choose an option:");
+        cns.println("1. Addition");
+        cns.println("2. Subtraction");
+        cns.println("3. Multiplication");
+        cns.println("4. Fibonacci");
+        cns.printf("Your choice: ");
+    }
+
+    public static void fiboLast(KonobloConsole cns) {
+        int x = cns.readInt(2, Integer.MAX_VALUE);
+        int a = 0, b = 1;
+        for (int i = 2; i <= x; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        cns.printf("Fibonacci(%d) = %d\n", x, b);
+    }
+
+    public static void fiboMenu(KonobloConsole cns) {
+        cns.printf("Do you want to see all steps (0 for no, 1 for yes): ");
+    }
+
+    public static void fiboAll(KonobloConsole cns) {
+        int x = cns.readInt(2, Integer.MAX_VALUE);
+        int a = 0, b = 1;
+        cns.printf("Fibonacci(0) = 0\n");
+        cns.printf("Fibonacci(1) = 1\n");
+        for (int i = 2; i <= x; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+            cns.printf("Fibonacci(%d) = %d\n", i, b);
+        }
+    }
+
     public static void main(String[] args) {
-        KonobloConsole console = new KonobloConsole();
-        console.run();
+        KonobloConsole cns = new KonobloConsole();
+
+        cns.setExitFunction(() -> {
+            cns.println("Thanks for using this program!");
+        });
+
+
+        cns.intMenu("#A", 1, 4, Test::mainMenu)
+            .option("#A1", Test::add, BACK)
+            .option("#A2", Test::sub, BACK)
+            .option("#A3", Test::mul, EXIT)
+            .intMenu("#A4", 0, 1, Test::fiboMenu)
+                .option("#A4.1", Test::fiboLast, EXIT)
+                .option("#A4.2", Test::fiboAll, EXIT);
+
+        cns.run();
+    }
+
+    public static void versionWithoutKonoblo() {
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        boolean keepRunning = true;
+
+        while (keepRunning) {
+            System.out.println("Hello please choose an option:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Fibonacci");
+            System.out.printf("Your choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.printf("Enter number #1: ");
+                    x = scanner.nextInt();
+                    System.out.printf("Enter number #2: ");
+                    y = scanner.nextInt();
+                    System.out.printf("%d + %d = %d\n", x, y, x+y);
+                    break;
+                case 2:
+                    System.out.printf("Enter number #1: ");
+                    x = scanner.nextInt();
+                    System.out.printf("Enter number #2: ");
+                    y = scanner.nextInt();
+                    System.out.printf("%d - %d = %d\n", x, y, x-y);
+                    break;
+                case 3:
+                    System.out.printf("Enter number #1: ");
+                    x = scanner.nextInt();
+                    System.out.printf("Enter number #2: ");
+                    y = scanner.nextInt();
+                    System.out.printf("%d * %d = %d\n", x, y, x*y);
+                    keepRunning = false;
+                    break;
+                case 4:
+                    do {
+                        System.out.printf("Enter an index for fibonacci (x>=2): ");
+                        x = scanner.nextInt();
+                    } while (x < 2);
+
+                    do {
+                        System.out.printf("Do you want to see all steps (0 for no, 1 for yes): ");
+                        y = scanner.nextInt();
+                    } while(y != 0 && y != 1);
+
+                    int a = 0, b = 1, next;
+                    if (y == 0) {
+                        // compute only fib(x)
+                        for (int i = 2; i <= x; i++) {
+                            next = a + b;
+                            a = b;
+                            b = next;
+                        }
+                        System.out.printf("Fibonacci(%d) = %d\n", x, b);
+                    } else {
+                        // print fib(0), fib(1), ..., fib(x)
+                        System.out.printf("Fibonacci(0) = 0\n");
+                        System.out.printf("Fibonacci(1) = 1\n");
+                        for (int i = 2; i <= x; i++) {
+                            next = a + b;
+                            a = b;
+                            b = next;
+                            System.out.printf("Fibonacci(%d) = %d\n", i, b);
+                        }
+                    }
+
+                    keepRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid option, please try again.");
+                    break;
+            }
+        }
+
+        System.out.println("Thanks for using this program!");
+        scanner.close();
     }
 
 }
